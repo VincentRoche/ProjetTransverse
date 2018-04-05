05799b18b87b67cc62a60010b45fbff8c45cafff
GaetanFaucherEfrei <gaetan.faucher@efrei.net>
2018-04-05 09:06:03 +0200

    un commentaire pour tester

---
+++ PjTransverse/src/eyjafjallajokull/projettransverse/test/Test.java
@@ -0,0 +1,12 @@
+package eyjafjallajokull.projettransverse.test;
+
+import eyjafjallajokull.projettransverse.model.Reseau;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Reseau reseau = new Reseau(500, 500);
+		
+	}
+
+}
