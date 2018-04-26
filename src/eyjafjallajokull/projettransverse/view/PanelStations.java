66226915a8609cdb0b88ed02ac76bc480551771b
VincentRoche <vin.roche@hotmail.fr>
2018-04-26 10:23:31 +0200

    Revert "Ajout des traits reliant les stations"
    This reverts commit 95746d02f95490ec8a1c3c2fa112b8e5580aaca2.

--- src/eyjafjallajokull/projettransverse/view/PanelStations.java
+++ src/eyjafjallajokull/projettransverse/view/PanelStations.java
@@ -1,13 +1,10 @@
 package eyjafjallajokull.projettransverse.view;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-import eyjafjallajokull.projettransverse.model.Arc;
 import eyjafjallajokull.projettransverse.model.Reseau;
 import eyjafjallajokull.projettransverse.model.Station;
 
@@ -19,7 +16,6 @@ public class PanelStations extends JPanel {
 	private final Reseau reseau;
 
 	private static final int TAILLE_ROND = 30;
-	private static final int EPAISSEUR_TRAIT = 3;
 
 	//Constructeurs
 	public PanelStations(Reseau reseau) {
@@ -27,28 +23,13 @@ public class PanelStations extends JPanel {
 	}
 
 	public void paintComponent(Graphics g) {
-		// Affichage de chaque arc
-		for (Arc a : reseau.getArcs())
-		{
-			Color Stationcolor = new Color(153, 0, 51);
-			g.setColor(Stationcolor) ;
-			int xb = a.getExtremite1().getCoordonneeX();
-			int yb = a.getExtremite1().getCoordonneeY();
-			int xf = a.getExtremite2().getCoordonneeX();
-			int yf = a.getExtremite2().getCoordonneeY();
-			Graphics2D g2 = (Graphics2D) g;
-			g2.setStroke(new BasicStroke(EPAISSEUR_TRAIT));
-			g2.setColor(Stationcolor);
-			g2.drawLine(xb, yb, xf, yf);
-
-		}
 		// Affichage de chaque station
 		for (Station s : reseau.getStations())
 		{
 			Color Stationcolor = new Color(153, 0, 51);
 			g.setColor(Stationcolor) ;
-			int xb = s.getCoordonneeX() - TAILLE_ROND / 2;
-			int yb = s.getCoordonneeY() - TAILLE_ROND / 2;
+			int xb = s.getCoordonneeX();
+			int yb = s.getCoordonneeY();
 			g.fillOval(xb, yb, TAILLE_ROND, TAILLE_ROND);
 			g.setColor(Color.WHITE);
 			int ecart = TAILLE_ROND / 6;
