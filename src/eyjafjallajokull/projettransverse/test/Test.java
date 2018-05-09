ea7ef93b651ea7473a59d0566a34c897641653c5
VincentRoche <vin.roche@hotmail.fr>
2018-05-09 17:12:21 +0200

    Indentation

--- src/eyjafjallajokull/projettransverse/test/Test.java
+++ src/eyjafjallajokull/projettransverse/test/Test.java
@@ -16,81 +16,81 @@ import java.io.IOException;
 public class Test {
 
 	public static Station recherche(Reseau reseau, String nom){
-		 for (Station s : reseau.getStations()){
+		for (Station s : reseau.getStations()){
 			if (s.getNom().equals(nom)){
 				return s;
 			}
-		  }
-		  return null;        
-	  }
-	
+		}
+		return null;        
+	}
+
 	public static void main(String[] args) {
 		int tailleMax = 500;
 		Reseau reseau = new Reseau(tailleMax, tailleMax);
-		
-                                    //Lecture fichier Station
-                                    String pathFichier="station.txt";
 
-                                    BufferedReader fluxEntree=null;
-                                    try {
-                                            fluxEntree = new BufferedReader(new FileReader(pathFichier));
+		//Lecture fichier Station
+		String pathFichier="station.txt";
 
-                                            String ligneLue = null;
-                                            String station [] = null;
-
-                                            while((ligneLue = fluxEntree.readLine())!=null){
-                                                      station = ligneLue.split(" ");
-                                                      
-                                                      reseau.ajouterStation(Integer.parseInt(station[1]), Integer.parseInt(station[2]), station[0]);                         
-                                            }
-                                    }
-                                    catch(IOException exc){
-                                            exc.printStackTrace();
-                                    }
-                                    finally{
-                                            try{
-                                                    if(fluxEntree!=null){
-                                                            /* Fermeture du flux vers le fichier */
-                                                            fluxEntree.close();
-                                                    }
-                                            }
-                                            catch(IOException e){
-                                                    e.printStackTrace();
-                                            }
-                                    }
-		//Lecture fichier trajet
-                pathFichier="trajet.txt";
-
-                /*BufferedReader */fluxEntree=null;
+		BufferedReader fluxEntree=null;
 		try {
-		    fluxEntree = new BufferedReader(new FileReader(pathFichier));
+			fluxEntree = new BufferedReader(new FileReader(pathFichier));
 
-		    String ligneLue = null;
-		    String trajet [] = null;
+			String ligneLue = null;
+			String station [] = null;
 
-		    while((ligneLue = fluxEntree.readLine())!=null){
-			      trajet = ligneLue.split(" ");
+			while((ligneLue = fluxEntree.readLine())!=null){
+				station = ligneLue.split(" ");
 
-			      for (int i=0; i < Integer.parseInt(trajet[2]); i++)
+				reseau.ajouterStation(Integer.parseInt(station[1]), Integer.parseInt(station[2]), station[0]);                         
+			}
+		}
+		catch(IOException exc){
+			exc.printStackTrace();
+		}
+		finally{
+			try{
+				if(fluxEntree!=null){
+					/* Fermeture du flux vers le fichier */
+					fluxEntree.close();
+				}
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		//Lecture fichier trajet
+		pathFichier="trajet.txt";
+
+		/*BufferedReader */fluxEntree=null;
+		try {
+			fluxEntree = new BufferedReader(new FileReader(pathFichier));
+
+			String ligneLue = null;
+			String trajet [] = null;
+
+			while((ligneLue = fluxEntree.readLine())!=null){
+				trajet = ligneLue.split(" ");
+
+				for (int i=0; i < Integer.parseInt(trajet[2]); i++)
 				{
 					reseau.ajouterVoyageur(recherche(reseau, trajet[0]), recherche(reseau, trajet[1]));
 				}                  
-			    }
-		    }
-		    catch(IOException exc){
-			    exc.printStackTrace();
-		    }
-		    finally{
-			    try{
-				    if(fluxEntree!=null){
-					    /* Fermeture du flux vers le fichier */
-					    fluxEntree.close();
-				    }
-			    }
-			    catch(IOException e){
-				    e.printStackTrace();
-			    }
-		    }
+			}
+		}
+		catch(IOException exc){
+			exc.printStackTrace();
+		}
+		finally{
+			try{
+				if(fluxEntree!=null){
+					/* Fermeture du flux vers le fichier */
+					fluxEntree.close();
+				}
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+		}
 
 		// Stations aléatoires
 		int nbStations = 10;
@@ -100,14 +100,14 @@ public class Test {
 			reseau.ajouterStation(rand.nextInt(tailleMax), rand.nextInt(tailleMax), "villejuif");
 		}*/
 		List<Station> stations = reseau.getStations();
-		
+
 		// Ligne qui relie tout
 		Ligne l = reseau.ajouterLigne(1);
 		for (int i=1; i < stations.size(); i++)
 		{
 			reseau.ajouterArc(stations.get(i), stations.get(i - 1), l);
 		}
-		
+
 		// Voyageurs aléatoires
 		/*for (Station s : stations)
 		{
@@ -117,7 +117,7 @@ public class Test {
 				reseau.ajouterVoyageur(s, stations.get(rand.nextInt(stations.size())));
 			}
 		}*/
-		
+
 		// Affichage
 		for (Station s : stations)
 		{
@@ -131,7 +131,7 @@ public class Test {
 		{
 			System.out.println(v);
 		}
-		
+
 		// Fenêtre
 		FenetrePlan f = new FenetrePlan(reseau);
 	}
